53255fd2cceb7e4458deb94d8f04d07099a269e5
Hytzy <62492146+Hytrzy@users.noreply.github.com>
2021-05-28 19:45:08 -0500

    update right arrow

--- app/src/main/java/com/jhaner/esp32/helper/Constant.java
+++ app/src/main/java/com/jhaner/esp32/helper/Constant.java
@@ -6,4 +6,6 @@ public class Constant {
     public static final String  METHOD_SHOWSHIELDS = "/script.php?method=showshields";
     public static final String  METHOD_SHOWMODULES= "/script.php?method=showmodules";
     public static final String  METHOD_SHOWDATA = "/script.php?method=showdata";
+
+    public static final String  SHOW = "/script.php?method=showdata";
 }
